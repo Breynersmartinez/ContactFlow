b344500647b75e32026a905a66dfe35681af26c9
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-09-02 04:12:33 +0000

    Se creo el proyecto para aneo de envio de meensaje del sitio web a mail

---
+++ src/main/java/com/example/ContactFlow/ContactFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContactFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ContactFlow/ContactFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ContactFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
